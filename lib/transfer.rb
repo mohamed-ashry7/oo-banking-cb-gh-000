d6894b8ac42d268acdcc0219aeb44b2ea11471b2
Mohamed Ashry <mohamedelashry7777@gmail.com>
2020-02-13 21:11:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
 
   def execute_transaction
-    if (amount >=sender.balance)
+    if amount >=sender.balance
       receiver.balance += amount
       sender.balance -= amount
       status = "successful"
