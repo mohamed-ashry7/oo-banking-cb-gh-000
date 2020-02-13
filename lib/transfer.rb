cb16757d356d54582acc36a6e80f0cae27bdef4a
Mohamed Ashry <mohamedelashry7777@gmail.com>
2020-02-13 21:22:30 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
 
   def execute_transaction
 
-    if amount <sender.balance && sta
+    if amount <sender.balance && status=="pending" 
       self.receiver.balance += self.amount
       self.sender.balance -= self.amount
       self.status = "complete"
