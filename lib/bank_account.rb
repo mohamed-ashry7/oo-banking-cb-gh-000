da139a14366f203490836f3af2c4544aef83de39
Mohamed Ashry <mohamedelashry7777@gmail.com>
2020-02-13 20:59:02 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,15 +1,13 @@
 class BankAccount
 
   attr_accessor :balance , :status 
-
+  attr_reader :name 
   def initialize(name)
     @name = name
     @balance = 1000
     @status = "open"
   end
-  def name
-    @name
-  end
+
   def deposit(money)
     @balance += money
   end
